1b194c4cfb842f21fcf73262dec1e14db7aec2c6
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-11-14 16:36:18 +0530

    Update Main.java
    update

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        B b = new B();
+        A b = new B();
         b.meth();
         System.out.println(b.meth());
         b.meth();
