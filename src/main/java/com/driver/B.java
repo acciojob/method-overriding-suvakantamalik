a62bcc28214bc35e6e4cc906589ca8d65f319b59
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-12-13 17:59:26 +0530

    meth added

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -1,7 +1,7 @@
 package com.driver;
 
 public class B extends A{
-    @Override
+//    @Override
     public String meth() {
         return "Method is overridden in Extended class B";
     }
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,9 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        B b = new B();
-        b.meth();
+        B obj = new B();
+        obj.meth();
+        B obj1 = new B();
+        obj1.meth();
     }
 }
