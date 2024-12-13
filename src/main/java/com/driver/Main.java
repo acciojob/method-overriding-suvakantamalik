52295c5192bc93a2fbddd7d1871ee16faba6044d
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-12-13 17:51:49 +0530

    meth added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,10 +2,7 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        A b = new B();
+        B b = new B();
         b.meth();
-        System.out.println(b.meth());
-        b.meth();
-        System.out.println(b.meth());
     }
 }
